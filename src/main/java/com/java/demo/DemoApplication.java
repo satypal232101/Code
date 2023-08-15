604dd2dca2f3f82cb3e25248f5fbc1c826436543
satypal232101 <satypal.pachu@gmail.com>
2023-08-15 19:55:45 +0530

    new controller class added

--- src/main/java/com/java/demo/DemoApplication.java
+++ src/main/java/com/java/demo/DemoApplication.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/getdetail")
-	public String getDetail() {
-		return "webcome :::";
-	}
+	
 
 }
